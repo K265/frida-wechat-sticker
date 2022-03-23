f761da3216857e838ab5b6d005fec245adf19de2
K265 <k.265@qq.com>
2022-03-23 10:04:08 +0800

    feat: add hook.js

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # frida-wechat-sticker
+
 提取 PC 微信加密的表情包 (V1MMWX)
+
+仅在 3.6.0.18 经过测试
+
+
+
+## Credits
+
+<https://github.com/kksanyu/frida_with_wechat_applet>
+
+[牛掰！把微信表情包导出GIF文件](https://jishuin.proginn.com/p/763bfbd35068)
+
+[获取、导出微信所有表情](https://blog.csdn.net/qq_43572067/article/details/100062493)
---
+++ hook.js
@@ -0,0 +1,67 @@
+var baseAddr = Module.findBaseAddress("VoipEngine.dll");
+console.log("VoipEngine.dll baseAddr: " + baseAddr);
+
+if (baseAddr) {
+  // isWxGF 函数偏移地址, 从 DLL 中查看
+  var isWxGF = baseAddr.add(0x2fd760);
+  console.log("isWxGF 函数地址: " + isWxGF);
+
+  // hook 函数, 监听参数
+  Interceptor.attach(isWxGF, {
+    onEnter: function (args) {
+      var gif = Memory.readByteArray(args[0], args[1].toInt32());
+      var filename = "r:/" + new Date().getTime() + ".gif";
+      var f = new File(filename, "wb");
+      f.write(gif);
+      f.flush();
+      f.close();
+    },
+  });
+} else {
+  console.log("VoipEngine.dll 模块未加载");
+}
+
+// /*
+//  * @name 微信小程序PC版 wxapkg提取
+//  * @author kksanyu
+//  * @url https://github.com/kksanyu/frida_with_wechat_applet
+//  */
+
+// var baseAddr = Module.findBaseAddress('WeChatAppHost.dll');
+// console.log('WeChatAppHost.dll baseAddr: ' + baseAddr);
+
+// if (baseAddr) {
+
+//     // EncryptBufToFile函数偏移地址, 从DLL中查看
+//     var EncryptBufToFile = baseAddr.add(0x1800F);
+//     console.log('EncryptBufToFile 函数地址: ' + EncryptBufToFile);
+
+//     // HOOK函数, 监听参数
+//     Interceptor.attach(EncryptBufToFile, {
+//         onEnter: function (args) {
+//             // 微信小程序AppId
+//             this.appId = ptr(args[0]).readPointer().readAnsiString();
+//             // 微信小程序本地缓存文件路径
+//             this.apkgFilePath = ptr(args[1]).readPointer().readAnsiString();
+//             // 小程序代码原始内容(未加密)
+//             this.originalData = Memory.readByteArray(args[2], args[3].toInt32());
+//         },
+//         onLeave: function (retval) {
+//             console.log('文件解密成功', this.apkgFilePath);
+
+//             // 将文件替换为未加密的wxapkg包
+//             var f = new File(this.apkgFilePath, 'wb');
+//             f.write(this.originalData);
+//             f.flush();
+//             f.close();
+
+//             // 释放内存
+//             delete this.appId;
+//             delete this.apkgFilePath;
+//             delete this.originalData;
+//         }
+//     });
+
+// } else {
+//     console.log('WeChatAppHost.dll 模块未加载, 请先打开界面中的小程序面板');
+// }
